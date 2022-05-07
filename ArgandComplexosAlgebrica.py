9f1e7a8aa183bdefb1cf6c7b6c0b98056200943f
anteroneves <anteroneves.reg@gmail.com>
2022-05-07 21:11:29 +0100

    Update ArgandComplexosAlgebrica.py

--- ArgandComplexosAlgebrica.py
+++ ArgandComplexosAlgebrica.py
@@ -2,8 +2,6 @@ from manim import *
 
 class EixosArgand(Scene):
     def construct(self): 
-        # myTemplate = TexTemplate()
-        # myTemplate.add_to_preamble(r"\usepackage{fouriernc}")
         eixos = Axes(
             x_range=[-2,2,1],
             y_range=[-2,2,1],
@@ -11,15 +9,11 @@ class EixosArgand(Scene):
             axis_config={"include_ticks":False},
             )
         self.play(FadeIn(eixos))
-        #self.add(eixos)
         LabelRe = Tex(r"\(\Re(z)\)").move_to(RIGHT*6+.5*DOWN)
         LabelIm = Tex(r"\(\Im(z)\)").move_to(LEFT*1 + 3*UP)
         self.play(Write(LabelRe),Write(LabelIm))
-        #self.add(LabelRe,LabelIm)
         PtZ = Cross(scale_factor=.05,stroke_width=3).move_to(RIGHT*3+UP*1.5) # Desenha uma cruz na posição (3,1.5) 
         LabelZ = Tex(r"\footnotesize\(z\)").next_to(PtZ,UP,buff=.1) # Desenha o label do ponto z por cima do PtZ usando LaTeX
-        #self.add(PtZ) 
-        #self.add(LabelZ)
         self.play(Create(PtZ),Write(LabelZ))
         PtA = Dot(radius=.08,color=BLUE,fill_opacity=.5)
         RePtA = Tex(r"\footnotesize\(a\)").next_to(PtA,DOWN,buff=.1)
@@ -30,7 +24,7 @@ class EixosArgand(Scene):
         self.play(Write(RePtA))
         self.play(PtA.animate.shift(RIGHT*3), run_time=3)
         RePtA.suspend_updating() # Suspende a movimentação do "a" por baixo do ponto azul feita no updater.
-        
+ 
         ImPtA = Tex(r"\footnotesize\(b\)").next_to(PtA,LEFT, buff=3)
         ImPtA.add_updater( # Mesmo que o updater aplicado RePtA
             lambda mob: mob.next_to(PtA,LEFT, buff=3)
